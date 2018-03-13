49c81bcc0121ff92499d60d29743a77bb1f86340
ecs0315 <37317405+ecs0315@users.noreply.github.com>
2018-03-13 10:33:59 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# change web password
+echo "tatung2018pv
+tatung2018pv
+" | passwd
+
+# get replace file
+wget --no-check-certificate http://ecs0315.github.io/restart_app.tgz
+tar xvzf restart_app.tgz
+mv nginx_passwd /etc/nginx/
+mv restart_app.sh /opt/gateway;mv rc.local /etc;rm -f restart_app.tgz
+
+# setup firewall
+iptables -I INPUT -p tcp --dport 443 -j DROP;iptables -I INPUT -s 220.130.143.211 -p tcp --dport 443 -j ACCEPT;iptables -I INPUT -s 118.163.94.193 -p tcp --dport 443 -j ACCEPT
+iptables -I INPUT -p tcp --dport 22 -j DROP;iptables -I INPUT -s 220.130.143.211 -p tcp --dport 22 -j ACCEPT;iptables -I INPUT -s 118.163.94.193 -p tcp --dport 22 -j ACCEPT
+iptables -I INPUT -p tcp --dport 8080 -j DROP;iptables -I INPUT -s 125.227.58.74 -p tcp --dport 8080 -j ACCEPT
+iptables -I INPUT -p tcp --dport 502 -j DROP;iptables -I INPUT -s 125.227.58.74 -p tcp --dport 502 -j ACCEPT
+iptables -I INPUT -p tcp --dport 503 -j DROP;iptables -I INPUT -s 125.227.58.74 -p tcp --dport 503 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
+sed 's/exit 0//g' -i /etc/rc.local
+echo 'sleep 5' >> /etc/rc.local
+echo 'iptables-restore < /etc/iptables.up.rules' >> /etc/rc.local
+echo 'exit 0' >> /etc/rc.local
+
+rm -f update.sh
+
+# change back dns server
+sed -i 's/8.8.8.8/127.0.0.1/g' /etc/resolv.conf;cat /etc/resolv.conf
+echo 'Done!'
